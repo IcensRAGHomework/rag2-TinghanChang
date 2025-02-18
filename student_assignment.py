b529af3c4654678d12864aee6c6054e0deab7dd4
Tinghan_Chang <Tinghan_Chang@asus.com>
2025-02-18 17:26:19 +0800

    test push

--- student_assignment.py
+++ student_assignment.py
@@ -1,13 +1,20 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
+from langchain_community.document_loaders import PyPDFLoader, PyMuPDFLoader
 
 q1_pdf = "OpenSourceLicenses.pdf"
 q2_pdf = "勞動基準法.pdf"
 
+def load_with_PyPdf(file_path):
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+    return docs
 
 def hw02_1(q1_pdf):
-    pass
+    pdf_file_path = "OpenSourceLicenses.pdf"
+    docs = load_with_PyPdf(pdf_file_path)
+    return docs
 
 def hw02_2(q2_pdf):
     pass
